f343eff3fec6a6b869d95549de7e8798da1539ad
sidul567 <sidulsam@gmail.com>
2024-06-21 04:27:45 +0600

    update app.ts file in server.

--- server/app.ts
+++ server/app.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 // CORS setup
 app.use(cors({
-    'origin': ['http://localhost:3000'],
+    'origin': ['http://localhost:3000', 'https://mern-lms-567.netlify.app'],
     'credentials': true
 }))
 
