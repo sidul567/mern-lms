0a5b9c2f261c78ebc029938ee2b83ed64aa87f7f
sidul567 <sidulsam@gmail.com>
2024-06-21 18:40:35 +0600

    update jwt.ts in server

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -2,12 +2,12 @@ import { Response } from "express";
 import { IUser } from "../models/user.model";
 import { redis } from "./redis";
 
-interface ITokenOptions{
-    expires: Date,
-    maxAge: number,
-    httpOnly: boolean,
-    sameSite: 'lax' | 'strict' | 'none' | undefined,
-    secure?: boolean,
+interface ITokenOptions {
+  expires: Date;
+  maxAge: number;
+  httpOnly: boolean;
+  sameSite: "lax" | "strict" | "none" | undefined;
+  secure?: boolean;
 }
 
 // parse environment variable for tokens
@@ -15,38 +15,42 @@ const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || "5");
 const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || "15");
 
 export const accessTokenOptions: ITokenOptions = {
-    expires: new Date(Date.now() + accessTokenExpire * 60 * 1000),
-    maxAge: accessTokenExpire * 60 * 1000,
-    httpOnly: true,
-    sameSite: 'lax',
-}
+  expires: new Date(Date.now() + accessTokenExpire * 60 * 1000),
+  maxAge: accessTokenExpire * 60 * 1000,
+  httpOnly: true,
+  sameSite: "lax",
+};
 
 export const refreshTokenOptions: ITokenOptions = {
-    expires: new Date(Date.now() + refreshTokenExpire * 60 * 1000),
-    maxAge: refreshTokenExpire * 60 * 1000,
-    httpOnly: true,
-    sameSite: 'lax',
-}
+  expires: new Date(Date.now() + refreshTokenExpire * 60 * 1000),
+  maxAge: refreshTokenExpire * 60 * 1000,
+  httpOnly: true,
+  sameSite: "none",
+};
 
-export const sendToken = async(user: IUser, statusCode: number, res: Response)=>{
-    const accessToken = user.signAccessToken();
-    const refreshToken = user.signRefreshToken();
+export const sendToken = async (
+  user: IUser,
+  statusCode: number,
+  res: Response
+) => {
+  const accessToken = user.signAccessToken();
+  const refreshToken = user.signRefreshToken();
 
-    // upload to redis session
-    redis.set(user._id, JSON.stringify(user));
+  // upload to redis session
+  redis.set(user._id, JSON.stringify(user));
 
-    // set secure for production
-    if(process.env.NODE_ENV === 'production'){
-        accessTokenOptions.secure = true;
-        refreshTokenOptions.secure = true;
-    }
+  // set secure for production
+  if (process.env.NODE_ENV === "production") {
+    accessTokenOptions.secure = true;
+    refreshTokenOptions.secure = true;
+  }
 
-    res.cookie("access_token", accessToken, accessTokenOptions);
-    res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+  res.cookie("access_token", accessToken, accessTokenOptions);
+  res.cookie("refresh_token", refreshToken, refreshTokenOptions);
 
-    res.status(statusCode).json({
-        success: true,
-        user,
-        token: accessToken,
-    });
-}
+  res.status(statusCode).json({
+    success: true,
+    user,
+    token: accessToken,
+  });
+};
